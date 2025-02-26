dd1cfb2f2762fa51a2df89cbebb483a65e2ed419
AleDulce13 <5323100074@correcaminos.uts.edu.mx>
2025-02-26 09:11:14 -0700

    Listo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ProductoComponent } from './listado-productos/producto/producto.component';
+import { MostrarMensajeComponent } from './mostrar-mensaje/mostrar-mensaje.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ProductoComponent],
+  imports: [ProductoComponent, MostrarMensajeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
