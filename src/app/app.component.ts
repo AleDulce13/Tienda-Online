ad310f287adf476e1314a47359529f6487f3697e
AleDulce13 <5323100074@correcaminos.uts.edu.mx>
2025-02-27 08:32:56 -0700

    Replicador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { ProductoComponent } from './listado-productos/producto/producto.component';
 import { MostrarMensajeComponent } from './mostrar-mensaje/mostrar-mensaje.component';
+import { ReplicadorComponent } from './replicador/replicador.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ProductoComponent, MostrarMensajeComponent],
+  imports: [ProductoComponent, MostrarMensajeComponent, ReplicadorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Tienda-Online';
+  titulo = 'Property Binding en Angular';
 }
